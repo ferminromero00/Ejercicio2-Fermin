cce144e25d9a78758650d5152151a9d9b51b0734
ferminromero00 <ferminromero2005@gmail.com>
2024-12-11 11:05:11 +0100

    Add files via upload

---
+++ Web/js/index.js
@@ -0,0 +1,11 @@
+// Agregar animación de entrada al desplazarse
+const productList = document.querySelectorAll('.product');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.style.animationDelay = `${Math.random() * 0.5}s`;
+            entry.target.classList.add('visible');
+        }
+    });
+}, {})
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "Región de AWS donde se desplegarán los recursos"
+  type        = string
+  default     = "us-east-1"
+}
